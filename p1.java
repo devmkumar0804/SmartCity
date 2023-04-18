b1aa1aaf05e20bd4cae40d6103ef066113c627ed
FallbackBrush <krishnangokul1029@gmail.com>
2023-04-18 08:27:05 +0530

    owl

--- p1.java
+++ p1.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 import javax.imageio.ImageIO;
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComponent;
@@ -263,6 +264,27 @@ class Citizen{
         System.out.println("payment made, data updated");
 
     }
+
+    public void makePaymentRide(String src,String dest) throws SQLException{
+        ResultSet r1;
+        double rpkm;
+        double dist;
+        double amt=0;
+        ResultSet rBank;
+        double newAmt=0;
+        rBank = s1.executeQuery("select * from dist where sourc="+"\""+src+"\""+" and destination="+"\""+dest+"\"");
+        while(rBank.next()){
+            System.out.println("Fare = "+rBank.getString("rateperkm"));
+            System.out.println("Distance = "+rBank.getDouble("distance"));
+            rpkm = rBank.getDouble("rateperkm");
+            dist = rBank.getDouble("distance");
+            amt=rpkm*dist;
+        }
+        newAmt=newAmt-amt;
+        int r = s1.executeUpdate("update bank set amount="+newAmt+" where citizen_id="+"\""+citizenID+"\"");
+        System.out.println("payment made, data updated");
+
+    }
 }
 
 class ImagePanel extends JComponent{
@@ -285,7 +307,7 @@ class CitizenView{
         ImageIcon img = new ImageIcon("C:\\Users\\gauta\\SmartCity\\background.jpg");
         frame1.setIconImage(img.getImage());
         frame1.setSize(1000, 1000);
-        frame1.setLayout(new GridLayout(10, 1,0,20));
+        frame1.setLayout(new BorderLayout(20, 30));
         frame1.setResizable(true);
         JLabel background = new JLabel(new ImageIcon("./background.jpg"));
         frame1.add(background);
@@ -295,15 +317,18 @@ class CitizenView{
 
 
 
-
+        JPanel headingPanel = new JPanel(new BorderLayout(20, 30));
         JLabel headingLabel = new JLabel("Hello There, "+c1.citizenName,0);
         //headingLabel.setBounds(150, 20, 200, 20);
         // headingLabel.setAlignmentX(0.5f);
         // headingLabel.setAlignmentY(0.5f);
-        frame1.add(headingLabel);
+        //frame1.add(headingLabel);
+        headingPanel.add(headingLabel,BorderLayout.NORTH);
         JLabel l1 = new JLabel("Welcome to the Smart City Manager",0);
         //l1.setBounds(100, 40, 300, 20);
-        frame1.add(l1);
+        headingPanel.add(l1,BorderLayout.SOUTH);
+        //headingPanel.set
+        //frame1.add(l1);
         JMenu m1 = new JMenu("Available Options", true);
 
         JButton b1 = new JButton("List Locations");
@@ -312,13 +337,26 @@ class CitizenView{
         JButton b4 = new JButton("Book A Ride");
         JButton b5 = new JButton("Make A Payment");
         JButton b6 = new JButton("List Your Utility Dues");
-        JPanel buttonPanel = new JPanel(new GridLayout(3, 2));
+        JPanel buttonPanel = new JPanel();
+       //buttonPanel.setBounds(300,  300, 500, 500);
         buttonPanel.add(b1);
         buttonPanel.add(b2);
         buttonPanel.add(b3);
         buttonPanel.add(b4);
         buttonPanel.add(b5);
         buttonPanel.add(b6);
+        // b1.setBounds(50, 200, 50, 20);
+        // b1.setBounds(110, 200, 50, 20);
+        // b1.setBounds(170, 200, 50, 20);
+        // b1.setBounds(230, 200, 50, 20);
+        // b1.setBounds(290, 200, 50, 20);
+        // b1.setBounds(350, 200, 50, 20);
+        // frame1.add(b1);
+        // frame1.add(b2);
+        // frame1.add(b3);
+        // frame1.add(b4);
+        // frame1.add(b5);
+        // frame1.add(b6);
         JTabbedPane tResult = new JTabbedPane();
         //m1.setLayout(new GridLayout(3, 2));
         //locs.setBounds(100, 70, 100, 20);
@@ -340,15 +378,16 @@ class CitizenView{
                         String f = s1+"\n"+s2+"\n"+s3+"\n"+s4+"\n";
                         JTextField tf1 = new JTextField(f);
                         p1.setLayout(new GridLayout(4, 1));
-                        tf1.setSize(600,300);
-                        p1.add(s1);
-                        p1.add(s2);
-                        p1.add(s3);
-                        p1.add(s4);
+                        p1.setBounds(100, 600, 600, 600);
+                        tf1.setSize(600,600);
+                        p1.add(s1, 0);
+                        p1.add(s2,1);
+                        p1.add(s3,2);
+                        p1.add(s4,3);
                         tResult.add("location "+(i+1),p1);
 
                     }
-                    frame1.add(tResult);
+                    frame1.add(tResult,BorderLayout.PAGE_END);
                 } catch (SQLException e1) {
                     // TODO Auto-generated catch block
                     e1.printStackTrace();
@@ -380,10 +419,40 @@ class CitizenView{
                         p1.add(s3);
                         p1.add(s4);
                         p1.add(s5);
-                        tResult.add("location "+(i+1),p1);
+                        tResult.add("Ride  "+(i+1),p1);
+                        
 
                     }
-                    frame1.add(tResult);
+                    //frame1.add(tResult,BorderLayout.PAGE_END);
+                    frame1.add(tResult,BorderLayout.PAGE_END);
+                    
+                } catch (SQLException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                }
+            }
+        });
+
+        b4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    tResult.removeAll();
+                    ArrayList locs1 = c1.listBank();
+                    //t1.setBounds(50, 150, 400, 300);
+                    for(int i=0;i<locs1.size();i++){
+                    
+                        JPanel p1 = new JPanel();
+                        JLabel s1 = new JLabel("Bank ID : "+locs1.get(0));
+                        JLabel s2 = new JLabel("Balance : "+locs1.get(1));
+
+                        p1.setLayout(new GridLayout(4, 1));
+                        p1.add(s1);
+                        p1.add(s2);
+                        tResult.add("Your Balance "+(i+1),p1);
+
+                    }
+                    frame1.add(tResult,BorderLayout.PAGE_END);
                 } catch (SQLException e1) {
                     // TODO Auto-generated catch block
                     e1.printStackTrace();
@@ -411,7 +480,45 @@ class CitizenView{
                         tResult.add("Your Balance "+(i+1),p1);
 
                     }
-                    frame1.add(tResult);
+                    frame1.add(tResult,BorderLayout.PAGE_END);
+                } catch (SQLException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                }
+            }
+        });
+
+        b6.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    tResult.removeAll();
+                    Double amt = c1.utilCalc();
+                    JPanel p1 = new JPanel();
+                        JLabel s1 = new JLabel("Amount Due : "+amt);
+                    p1.add(s1);
+                    tResult.add(p1);
+                    //t1.setBounds(50, 150, 400, 300);
+                    frame1.add(tResult,BorderLayout.PAGE_END);
+                } catch (SQLException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                }
+            }
+        });
+
+        b5.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    tResult.removeAll();
+                    c1.makePayment();
+                    JPanel p1 = new JPanel();
+                        JLabel s1 = new JLabel("Utility bills payed");
+                    p1.add(s1);
+                    tResult.add(p1);
+                    //t1.setBounds(50, 150, 400, 300);
+                    frame1.add(tResult,BorderLayout.PAGE_END);
                 } catch (SQLException e1) {
                     // TODO Auto-generated catch block
                     e1.printStackTrace();
@@ -424,7 +531,10 @@ class CitizenView{
         // frame1.add(b4);
         // frame1.add(b5);
         // frame1.add(b6);
-        frame1.add(buttonPanel);
+        headingPanel.setVisible(true);
+        frame1.add(headingPanel,BorderLayout.PAGE_START);
+        buttonPanel.setVisible(true);
+        frame1.add(buttonPanel,BorderLayout.CENTER);
         frame1.setVisible(true);
 
         frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
